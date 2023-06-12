c5a8344dd071c52cd9539398943d81d2435e6ab8
amx100 <amx281@gmail.com>
2023-06-12 16:30:23 +0200

    Refactured Appointment Service

--- Klinika.API/Contracts/Service/IAppointmentService.cs
+++ Klinika.API/Contracts/Service/IAppointmentService.cs
@@ -1,12 +1,15 @@
 ﻿using Klinika.API.Data.Dto.Appointment;
 using Klinika.API.Data.Dto;
+using Klinika.API.Data.Dto.Patient;
 
 namespace Klinika.API.Contracts.Service
 {
 	public interface IAppointmentService
 	{
+		Task<IEnumerable<PatientReadOnlyDto>> GetPatientsByDoctorId(int doctorId);
 		Task<IEnumerable<AppointmentReadOnlyDto>> GetAllAppointments();
 		Task<AppointmentReadOnlyDto> GetAppointmentById(int appointmentId);
+		
 		Task<ResponseDto> CreateAppointment(AppointmentCreateDto appointmentDto);
 		Task<ResponseDto> UpdateAppointment(int appointmentId, AppointmentUpdateDto appointmentDto);
 		Task<bool> DeleteAppointment(int appointmentId);
