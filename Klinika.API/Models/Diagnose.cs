7cf31c163c8e9c762053fbbb8ed996ff1ad222a1
amx100 <amx281@gmail.com>
2023-06-12 16:33:54 +0200

    Refactured Structure

--- Klinika.API/Models/Diagnose.cs
+++ Klinika.API/Models/Diagnose.cs
@@ -2,12 +2,14 @@
 
 namespace Klinika.API.Models
 {
+
 	public class Diagnose
 	{
 		[Key]
-		public int DiagnoseID { get; set; }
+		public int diagnoseID { get; set; }
 		public string name { get; set; } = string.Empty;
 		public string description { get; set; } = string.Empty;
-
+		public int patientID { get; set; }
+		public Patient Patient { get; set; }
 	}
 }
